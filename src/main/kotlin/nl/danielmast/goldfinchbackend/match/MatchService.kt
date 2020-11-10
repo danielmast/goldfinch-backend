b85db5a0d807ff97617a8c79a498113880bc2d2e
danielmast <danielmast92@gmail.com>
2020-11-10 19:02:40 +0100

    Delete Match domain because not yet needed

--- src/main/kotlin/nl/danielmast/goldfinchbackend/match/MatchService.kt
+++
@@ -1,9 +0,0 @@
-package nl.danielmast.goldfinchbackend.match
-
-import nl.danielmast.goldfinchbackend.user.UserService
-import org.springframework.stereotype.Service
-
-@Service
-class MatchService(private val userService: UserService, private val matchRepository: MatchRepository) {
-//    fun getUserById(id: Long): User? = userRepository.getUserById(id)
-}
